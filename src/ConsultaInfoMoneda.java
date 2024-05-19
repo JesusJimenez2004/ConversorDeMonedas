107c244421a02c7171ea4cd2dda2dc2dbe22b973
Jesús Jiménez Álvarez <jesus.jimenezal24@gmail.com>
2024-05-18 23:52:01 -0600

    Se agrega el proyecto

--- src/ConsultaInfoMoneda.java
+++ src/ConsultaInfoMoneda.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ConsultaInfoMoneda {
-}
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaInfoMoneda {
+
+        public Conversor buscaMoneda(String denominacionBase){
+            URI direccion = URI.create("https://v6.exchangerate-api.com/v6/00073386ba4eb520ab7a0a01/latest/"+denominacionBase);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(direccion)
+                    .build();
+
+
+            try {
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+                return new Gson().fromJson(response.body(), Conversor.class);
+            } catch (Exception e) {
+                throw new RuntimeException("No se pudo realizar la conversión");
+            }
+
+        }
+    }
+
+
+
--- src/Conversion.java
+++ src/Conversion.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Conversion {
+import java.time.LocalDateTime;
+
+public class Conversion {
+
+    private String monedaBase;
+    private String monedaObjetivo;
+    private Double cantidadACambiar;
+    private Double cantidadEnMonedaObjetivo;
+    private LocalDateTime tiempo;
+
+
+    public Conversion(String monedaObjetivo, String monedaBase, Double cantidadACambiar, Double cantidadEnMonedaObjetivo) {
+        this.monedaObjetivo = monedaObjetivo;
+        this.monedaBase = monedaBase;
+        this.cantidadACambiar = cantidadACambiar;
+        this.cantidadEnMonedaObjetivo = cantidadEnMonedaObjetivo;
+        this.tiempo = LocalDateTime.now();
+    }
+
+    public String getMonedaBase() {
+        return monedaBase;
+    }
+
+    public void setMonedaBase(String monedaBase) {
+        this.monedaBase = monedaBase;
+    }
+
+    public Double getCantidadACambiar() {
+        return cantidadACambiar;
+    }
+
+    public void setCantidadACambiar(Double cantidadACambiar) {
+        this.cantidadACambiar = cantidadACambiar;
+    }
+
+    public String getMonedaObjetivo() {
+        return monedaObjetivo;
+    }
+
+    public void setMonedaObjetivo(String monedaObjetivo) {
+        this.monedaObjetivo = monedaObjetivo;
+    }
+
+    public Double getCantidadEnMonedaObjetivo() {
+        return cantidadEnMonedaObjetivo;
+    }
+
+    public void setCantidadEnMonedaObjetivo(Double cantidadEnMonedaObjetivo) {
+        this.cantidadEnMonedaObjetivo = cantidadEnMonedaObjetivo;
+    }
+
+    public LocalDateTime getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(LocalDateTime tiempo) {
+        this.tiempo = tiempo;
+    }
 }
